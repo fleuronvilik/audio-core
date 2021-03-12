5533a6b8d630f4c0fdaba594adb2ce196616666d
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-03-12 12:12:46 +0100

    Fix path for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: `${__dirname}/src/images/`,
       },
       __key: "images",
     },
