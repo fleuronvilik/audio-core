4e141d211c90c9d78794c2a2c030ab07f6c7a1cf
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-03-12 12:45:37 +0100

    Comment out gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    // "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
